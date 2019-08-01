be5b04b73a321256b0d53cd230e8d72f08a610d9
Denise NGUYEN <denise.nguyen@epitech.eu>
2019-08-01 19:56:09 +0200

    wip: configure pom

---
+++ src/test/scala/com/dng/ebengine/EbEngineTest.scala
@@ -0,0 +1,11 @@
+package com.dng.ebengine
+
+class EbEngineTest extends UnitSpec {
+  describe("A Set") {
+    describe("when max timestamp is") {
+      it("should return the max timestamp ") {
+        assert(Set.empty.size == 0)
+      }
+    }
+  }
+}
